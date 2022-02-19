624dac453cbb002c6306c87e1761a2002b409da2
Ulises688apaza <ulisesapaza.280@gmail.com>
2022-02-19 14:09:50 -0300

    feature: add pokedex api

---
+++ netlify/functions/helloworld.js
@@ -0,0 +1,11 @@
+/** @format */
+exports.handler = async function () {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "hello world!"
+            }
+        )
+    };
+};
